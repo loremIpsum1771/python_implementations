fe45bdcfeb250f5c55726320d8d1f39b9abd0266
Ceyron Hawthorne <loremIpsum1771@users.noreply.github.com>
2017-03-01 14:10:43 -0500

    Update hashtable.py

--- hashtable.py
+++ hashtable.py
@@ -1,5 +1,5 @@
 import os
-from enum import Enum
+
 
 def calc_total(a,b):
     return a+b
@@ -8,171 +8,119 @@ def calc_multiply(a,b):
     return a*b
 
 
+class Hashtable(object):
+   COUNT = 0 
+   MINSIZE = 8
+   def __init__(self, **initial_values):
+       # self._buckets = [None] * self.MINSIZE
+       self._buckets = [[None] for i in range(self.MINSIZE)]
 
-
-##Generate a hash given the word 'foo'
-#key = 'foo'
-#value = 'hello world'
-#hash(key) 
-
-##convert to binary, slice last 3 bits 
-#bits = bin(hash(key))[-3]
-
-#idx = int(bits,2)
-
-##Create 3 bits worth of buckets
-#buckets = [None] * 8
-#buckets[idx] = 'hello world'
-
-##load back data
-#buckets[int(bin(hash('foo'))[-3],2)]
-
-
-
-#class Hashtable(object):
-#    TOMBSTONE = '<Tombstone>'
-#    MINSIZE = 8
-#    def __init__(self, **initial_values):
-#        self._buckets = [None] * self.MINSIZE
-
-#        for k, v in initial_values.iteritems():
-#            self.insert(k,v)
+       for k, v in initial_values.iteritems():
+           self.insert(k,v)
     
-#    def get(self,key):
-#        h = hash(key)
-#        idx = h & self.mask
+   def get(self,key):
+       h = hash(key)
+       idx = h & self.mask
 
-#        bucket = self._bukcets[idx]
+       bucket = self._buckets[idx]
 
-#        # start naive probe, if the bucket is existant but not correct key
-#        if bucket and bucket[1] != key:
-#            idx = self._locate(key, h)
-#        return self._buckets[idx][2] if self._bukcets[idx] else None
+       # search for the key in the chain
+       if bucket and bucket[0][0] != key:
+           for i in range(len(bucket)):
+           		if bucket[i][0] == key:
+           			return bucket[i][1]
+       return self._buckets[idx][1] if self._buckets[idx][0] else None
 
-#    def insert(self, key, value):
-#        # If the buckets seem over-utilized, grow and re-index
-#        if self.utilization >= 0.75:
-#            self._resize(len(self._buckets) * 4)
+   def insert(self, key, value):
+       # If the buckets seem over-utilized, grow and re-index
+       # if self.utilization >= 0.75:
+       #     self._resize(len(self._buckets) * 4)
+       if(self.get(key) == None):
+           COUNT += 1
+           self._buckets[idx] = (key,value)	
+       if( COUNT/len(self._buckets) >= 0.75 ):
+           self._resize(len(self._buckets) * 4)
+       h = hash(key)
+       idx = h & self.mask
+       self._buckets[idx].append((key,value))
 
-#        h = hash(key)
-#        idx = h & self.mask
+   def delete(self, key):
+       '''Simple delete method to delete buckets by key '''
 
-#        #If the initial probe returned a bucket (is occupied), probe randomly for the next slot
+       h = hash(key)
+       idx = h & self.mask
 
-#        if self._buckets[idx]:
-#            idx = self._locate(key, h)
-        
-#        self._buckets[idx] = (h, key, value)
+       bucket = self._buckets[idx]
+       if bucket and bucket[0][0] != key:
+           for i in range(len(bucket)):
+           		if bucket[i][0] == key:
+           			bucket[i] = None
 
-#    def delete(self, key):
-#        '''Simple delete method to delete buckets by key '''
+       #self._buckets[idx] = self.TOMBSTONE
 
-#        h = hash(key)
-#        idx = h & self.mask
-
-#        bucket = self._buckets[idx]
-#        if bucket and bucket[1] != key:
-#            idx = self._locate(key, h)
-
-#        self._buckets[idx] = self.TOMBSTONE
-
-#        # If the buckets seem under-utilized, shrink and re-index
-#        if 0 < self.utilization <= 0.16 and len(self._buckets) > self.MINSIZE:
-#            self._resize(len(self._buckets) / 4)
+       # If the buckets seem under-utilized, shrink and re-index
+       # if 0 < self.utilization <= 0.16 and len(self._buckets) > self.MINSIZE:
+       #     self._resize(len(self._buckets) / 4)
 
 
-#    def _locate(self, key, h):
-#        idx = h & self.mask
-#        bucket = self.mask
+   def _locate(self, key, h):
+       idx = h & self.mask
+       bucket = self.mask
 
-#        perturb = h 
-#        while True:
-#            idx = ((idx >> 2) + idx + perturb + 1)& self.mask
-#            bucket = self._buckets[idx]
+       self._buckets[idx].append()
 
-#            if not bucket or bucket[1] == key:
-#                return idx
 
-#            perturb >>= 5
+       # perturb = h 
+       # while True:
+       #     idx = ((idx >> 2) + idx + perturb + 1)& self.mask
+       #     bucket = self._buckets[idx]
+
+       #     if not bucket or bucket[1] == key:
+       #         return idx
+
+       #     perturb >>= 5
     
 
-#    def _resize(self, size):
-#        old_buckets = self._buckets
-#        self._buckets = [None] * size
+   def _resize(self, size):
+       old_buckets = self._buckets
+       self._buckets = [[None] for i in range(size)]
 
-#        #Make sure the old data gets re-indexed into the new store
-#        for bucket in [b for b in old_buckets if b]:
-#            self.inesrt(bucket[1], bucket[2])
+       #Make sure the old data gets re-indexed into the new store
+       #for bucket in [b for b in old_buckets if b]:
+       for i in range(len(old_buckets)):
+       		for j in range(len(old_buckets[i])):
+       			bucket = old_buckets[i][j]
+           		self.insert(bucket[0], bucket[1])
 
-#    @property
-#    def mask(self):
-#        return len(self._buckets) - 1
+   @property
+   def mask(self):
+       return len(self._buckets) - 1
 
-#    @property
-#    def utilitzation(self):
-#        '''Calculate the number of buckets that are actually populated '''
-#        try:
-#            return float(len(self))/float(len(self._buckets))
-#        except ZeroDivisionError:
-#            return 0
-#    def __len__(self):
-#        '''Len should return the number of non-tombstoned and populated records'''
-#        return len([b for b in self._buckets if b and b != self.TOMBSTONE])
+   @property
+   def utilization(self):
+       '''Calculate the number of buckets that are actually populated '''
+       try:
+           return float(len(self))/float(len(self._buckets))
+       except ZeroDivisionError:
+           return 0
+   def __len__(self):
+       '''Len should return the number of non-tombstoned and populated records'''
+       chainSum = 0
+       for i in self._buckets:
+       		chainSum += len(i)
+       return chainSum
 
-#    def __setitem__(self, key, val):
-#        self.insert(key, val)
+   def __setitem__(self, key, val):
+       self.insert(key, val)
 
-#    def __getitem__(self, key):
-#        val = self.get(key)
-#        if val:
-#            return val
-#        else:
-#            raise KeyError
+   def __getitem__(self, key):
+       val = self.get(key)
+       if val:
+           return val
+       else:
+           raise KeyError
 
-#    def __delitem__(self, key):
-#        self.delete(key)
-
-#h = HashTable(one="one", two="two", three="three", four="four", five="five",
-#              six="six", seven="seven", eight="eight", nine="nine")
-
-#print(len(h._buckets))  # 32 - buckets have grown to accomodate items
-
-## Remove a bunch of the keys
-#del h['one']
-#del h['two']
-#del h['three']
-#del h['four']
-#del h['five']
-#del h['six']
-#del h['seven']
-#del h['eight']
-
-#print(len(h._buckets))  # 8 - buckets has now shrunk due to utilization
-#h['ten'] = 10
-#print(h.get('one'))  # None -
-#print(h.get('ten'))  # 10
+   def __delitem__(self, key):
+       self.delete(key)
 
 
-
-
-#EntryType = Enum('EntryType' ,'ACTIVE EMPTY DELETED')
-#class Hashtable2:
-    
-#    def __init__(self, capacity = 101):
-#        self.array = []
-#        self.currentSize = 0
-#        self.capacity = capacity
-#        for i in range(0,size):
-#            array.append(hashEntry)
-
-    
-    
-    
-
-#    def find(self, obj):
-#        i = hash(obj) % len(array)
-#        while True:
-#            if array[i].info == 
-#    def insert(self, obj):
-
-#    def delete(self, obj):
