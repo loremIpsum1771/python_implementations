95d8eacfc0b50f0418d920f6611a189158aa1cc4
Ceyron Hawthorne <loremIpsum1771@users.noreply.github.com>
2017-03-02 13:52:33 -0500

    Update test_hashtable.py

--- test_hashtable.py
+++ test_hashtable.py
@@ -1,54 +1,73 @@
 import hashtable
 
 def test_init():
-	'''test initialization of hashtable'''
-	h = hashtable.Hashtable(one="one", two="two", three="three", four="four", five="five", six="six", seven="seven", eight="eight", nine="nine")
-	assert len(h._buckets)  ==  32 # 32 - buckets have grown to accomodate items
+    '''test initialization of hashtable'''
+    minsize = 8
+    h = hashtable.Hashtable(minsize,one="one", two="two", three="three", four="four", five="five", six="six", seven="seven", eight="eight", nine="nine")
+    assert len(h._buckets)  ==  8 and  h.CHAIN_ELEMENTS == 9
 
 
 
-def test_find():
-	'''test location of keys'''
-	h = hashtable.Hashtable(one="one", two="two", three="three", four="four", five="five", six="six", seven="seven", eight="eight", nine="nine")
-	assert h.get('four') == 'four'
+def test_get():
+    '''test location of keys'''
+    minsize = 8
+    h = hashtable.Hashtable(minsize,one="one", two="two", three="three", four="four", five="five", six="six", seven="seven", eight="eight", nine="nine")
+    assert h.get('four') == 'four'
 
 def test_delete():
-	''' test deletion of keys'''
-	# Remove a bunch of the keys
-	h = hashtable.Hashtable(one="one", two="two", three="three", four="four", five="five", six="six", seven="seven", eight="eight", nine="nine")
-	del h['one']
-	del h['two']
-	del h['three']
-	del h['four']
-	del h['five']
-	del h['six']
-	del h['seven']
-	del h['eight']
-
-	assert len(h._buckets) == 8 # 8 - buckets has now shrunk due to utilization
+    ''' test deletion of keys'''
+    minsize = 8
+    # Remove a bunch of the keys
+    h = hashtable.Hashtable(minsize,one="one", two="two", three="three", four="four", five="five", six="six", seven="seven", eight="eight", nine="nine")
+    del h['one']
+    del h['two']
+    del h['three']
+    del h['four']
+    del h['five']
+    del h['six']
+    del h['seven']
+    del h['eight']
+    assert h.CHAIN_ELEMENTS == 1 # 8 - buckets has now shrunk due to utilization
 
 def test_insert_after_del():
-	'''test insertion of keys after deletion'''
-	# Remove a bunch of the keys
-	h = hashtable.Hashtable(one="one", two="two", three="three", four="four", five="five", six="six", seven="seven", eight="eight", nine="nine")
-	del h['one']
-	del h['two']
-	del h['three']
-	del h['four']
-	del h['five']
-	del h['six']
-	del h['seven']
-	del h['eight']
-
-	#print(len(h._buckets))  # 8 - buckets has now shrunk due to utilization
-	h['ten'] = 10
-	assert h.get('ten') == 10  # 10
+    '''test insertion of keys after deletion'''
+    minsize = 8
+    # Remove some of the keys
+    h = hashtable.Hashtable(minsize,one="one", two="two", three="three", four="four", five="five", six="six", seven="seven", eight="eight", nine="nine")
+    del h['one']
+    del h['two']
+    del h['three']
+    del h['four']
+    del h['five']
+    del h['six']
+    del h['seven']
+    del h['eight']
+    h['ten'] = 10
+    assert h.get('ten') == 10  
     
     
    
 def test_resize():
-    h = hashtable.Hashtable(one="one", two="two", three="three", four="four", five="five", six="six", seven="seven", eight="eight", nine="nine")
-    h.MINSIZE = 3
+    '''test resizing of hashtable once load factor is reached'''
+    minsize = 3
+    h = hashtable.Hashtable(minsize)
+    h['one'] = "one"
+    h['two'] = "two"
+    h['three'] = "three"
+    h['four'] = "four"
+    h['five'] = "five"
+    h['six'] = "six"
+    h['seven'] = "seven"
+    h['eight'] = "eight"
+    h['nine'] = "nine"
     h['ten'] = 10
     #After resize, the number of (active) chains in the table should be 4 and total chain elements should be 10
     assert h.CHAIN_ELEMENTS == 10 and h.NUM_CHAINS == 4
+
+def test_add_duplicate():
+    '''test if the hashtable handles duplicates'''
+    minsize = 8
+    h  = hashtable.Hashtable(minsize,one="one", two="two", three="three", four="four", five="five", six="six", seven="seven", eight="eight", nine="nine")
+    h["one"] = 17
+    assert h.get("one") == 17
+
