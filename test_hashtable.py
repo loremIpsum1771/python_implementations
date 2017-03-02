9a226d4aeac4e4b2a516b2c13040c8dff5ac516b
Ceyron Hawthorne <loremIpsum1771@users.noreply.github.com>
2017-03-02 02:19:24 -0500

    Update test_hashtable.py

--- test_hashtable.py
+++ test_hashtable.py
@@ -1,5 +1,51 @@
 import hashtable
 
+def test_init():
+	'''test initialization of hashtable'''
+	h = hashtable.Hashtable(one="one", two="two", three="three", four="four", five="five", six="six", seven="seven", eight="eight", nine="nine")
+	assert len(h._buckets)  ==  32 # 32 - buckets have grown to accomodate items
+
+
+
+def test_find():
+	'''test location of keys'''
+	h = hashtable.Hashtable(one="one", two="two", three="three", four="four", five="five", six="six", seven="seven", eight="eight", nine="nine")
+	assert h.get('four') == 'four'
+
+def test_delete():
+	''' test deletion of keys'''
+	# Remove a bunch of the keys
+	h = hashtable.Hashtable(one="one", two="two", three="three", four="four", five="five", six="six", seven="seven", eight="eight", nine="nine")
+	del h['one']
+	del h['two']
+	del h['three']
+	del h['four']
+	del h['five']
+	del h['six']
+	del h['seven']
+	del h['eight']
+
+	assert len(h._buckets) == 8 # 8 - buckets has now shrunk due to utilization
+
+def test_insert_after_del():
+	'''test insertion of keys after deletion'''
+	# Remove a bunch of the keys
+	h = hashtable.Hashtable(one="one", two="two", three="three", four="four", five="five", six="six", seven="seven", eight="eight", nine="nine")
+	del h['one']
+	del h['two']
+	del h['three']
+	del h['four']
+	del h['five']
+	del h['six']
+	del h['seven']
+	del h['eight']
+
+	#print(len(h._buckets))  # 8 - buckets has now shrunk due to utilization
+	h['ten'] = 10
+	assert h.get('ten') == 10  # 10
+    
+    
+   
 def test_resize():
     h = hashtable.Hashtable(one="one", two="two", three="three", four="four", five="five", six="six", seven="seven", eight="eight", nine="nine")
     h.MINSIZE = 3
