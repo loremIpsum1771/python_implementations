3034581b616ff379309c58cda037b12258680913
Ceyron Hawthorne <loremIpsum1771@users.noreply.github.com>
2017-03-02 02:13:35 -0500

    Update hashtable.py

--- hashtable.py
+++ hashtable.py
@@ -1,19 +1,16 @@
 import os
 
 
-def calc_total(a,b):
-    return a+b
-
-def calc_multiply(a,b):
-    return a*b
 
 
 class Hashtable(object):
-   COUNT = 0 
-   MINSIZE = 8
+   NUM_CHAINS = 0 
+   CHAIN_ELEMENTS = 0
+   MINSIZE = 3
    def __init__(self, **initial_values):
        # self._buckets = [None] * self.MINSIZE
-       self._buckets = [[None] for i in range(self.MINSIZE)]
+       
+       self._buckets = [[] for i in range(self.MINSIZE)]
 
        for k, v in initial_values.iteritems():
            self.insert(k,v)
@@ -29,20 +26,27 @@ class Hashtable(object):
            for i in range(len(bucket)):
            		if bucket[i][0] == key:
            			return bucket[i][1]
-       return self._buckets[idx][1] if self._buckets[idx][0] else None
+       #print "self._buckets[idx][1]: " + self._buckets[idx][0][1] + " self._buckets[idx][0]: " +self._buckets[idx][0][0]
+       return bucket[0][1] if bucket[0] else None
 
    def insert(self, key, value):
        # If the buckets seem over-utilized, grow and re-index
        # if self.utilization >= 0.75:
        #     self._resize(len(self._buckets) * 4)
-       if(self.get(key) == None):
-           COUNT += 1
-           self._buckets[idx] = (key,value)	
-       if( COUNT/len(self._buckets) >= 0.75 ):
-           self._resize(len(self._buckets) * 4)
        h = hash(key)
        idx = h & self.mask
+       if(len(self._buckets[idx]) == 0 ):
+           self.NUM_CHAINS += 1
+     
+       self.CHAIN_ELEMENTS +=1
+           
+       
+       
+       print "num chains: " + str(self.NUM_CHAINS) + " num chain elements: " + str(self.CHAIN_ELEMENTS)
+       #print key
        self._buckets[idx].append((key,value))
+       if( self.CHAIN_ELEMENTS/self.NUM_CHAINS >= 5 ):
+           self._resize(len(self._buckets) * 4)
 
    def delete(self, key):
        '''Simple delete method to delete buckets by key '''
@@ -53,8 +57,13 @@ class Hashtable(object):
        bucket = self._buckets[idx]
        if bucket and bucket[0][0] != key:
            for i in range(len(bucket)):
-           		if bucket[i][0] == key:
-           			bucket[i] = None
+               if bucket[i] != None:
+                   if bucket[i][0] == key:
+                       if len(bucket[i]) <2:
+                           self.NUM_CHAINS -=1
+                       bucket[i] = None
+                       self.CHAIN_ELEMENTS -=1
+                       
 
        #self._buckets[idx] = self.TOMBSTONE
 
@@ -63,46 +72,29 @@ class Hashtable(object):
        #     self._resize(len(self._buckets) / 4)
 
 
-   def _locate(self, key, h):
-       idx = h & self.mask
-       bucket = self.mask
-
-       self._buckets[idx].append()
-
-
-       # perturb = h 
-       # while True:
-       #     idx = ((idx >> 2) + idx + perturb + 1)& self.mask
-       #     bucket = self._buckets[idx]
-
-       #     if not bucket or bucket[1] == key:
-       #         return idx
-
-       #     perturb >>= 5
-    
-
    def _resize(self, size):
        old_buckets = self._buckets
-       self._buckets = [[None] for i in range(size)]
-
+       self._buckets = [[] for i in range(size)]
+       print "before resize: \n num chains: " + str(self.NUM_CHAINS) + " num chain elements: " + str(self.CHAIN_ELEMENTS) + " total chains possible: " + str(len(old_buckets))
+       print(old_buckets)
+       self.NUM_CHAINS = 0
+       self.CHAIN_ELEMENTS = 0
+       print "after reset: \n num chains: " + str(self.NUM_CHAINS) + " num chain elements: " + str(self.CHAIN_ELEMENTS)
        #Make sure the old data gets re-indexed into the new store
        #for bucket in [b for b in old_buckets if b]:
        for i in range(len(old_buckets)):
-       		for j in range(len(old_buckets[i])):
-       			bucket = old_buckets[i][j]
-           		self.insert(bucket[0], bucket[1])
-
+           for j in range(len(old_buckets[i])):
+               if old_buckets[i][j] != None:
+                   bucket = old_buckets[i][j]
+                   self.insert(bucket[0], bucket[1])
+       print "after rehash: \n num chains: " + str(self.NUM_CHAINS) + " num chain elements: " + str(self.CHAIN_ELEMENTS) + " total chains possible: " + str(len(self._buckets))
+       print "after resize: \n"
+       print(self._buckets)
    @property
    def mask(self):
        return len(self._buckets) - 1
 
-   @property
-   def utilization(self):
-       '''Calculate the number of buckets that are actually populated '''
-       try:
-           return float(len(self))/float(len(self._buckets))
-       except ZeroDivisionError:
-           return 0
+   
    def __len__(self):
        '''Len should return the number of non-tombstoned and populated records'''
        chainSum = 0
@@ -123,4 +115,31 @@ class Hashtable(object):
    def __delitem__(self, key):
        self.delete(key)
 
+h = Hashtable(one="one", two="two", three="three", four="four", five="five", six="six", seven="seven", eight="eight", nine="nine")
+print "before delete: \n"
+#print(h._buckets)  # 32 - buckets have grown to accomodate items
+
+## Remove a bunch of the keys
+#del h['one']
+#del h['two']
+#del h['three']
+#del h['four']
+#del h['five']
+#del h['six']
+#del h['seven']
+#del h['eight']
+#print "after delete: \n"
+#print(h._buckets)
+
+#print(len(h._buckets))  # 8 - buckets has now shrunk due to utilization
+h['ten'] = 10
+h['eleven'] = 11
+h['twelve'] = 12
+h['thirteen'] = 13
+h['fourteen'] = 14
+h['fifteen'] = 15
+print(h.get('one'))  # None -
+
+print(h._buckets)
+print(h.get('ten'))  # 10
 
